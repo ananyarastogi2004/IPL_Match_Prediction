b988263ff47c6fc7237bea33e246e2836174302e
Ananya Rastogi <122351997+ananyarastogi2004@users.noreply.github.com>
2023-09-09 14:10:07 +0530

    Add files via upload

---
+++ IPL_MLmodel.py
@@ -0,0 +1,190 @@
+
+
+#import the libraries
+
+import math
+import numpy as np
+import pickle
+import streamlit as st
+import pandas as pd
+
+#SET PAGE WIDE
+st.set_page_config(page_title='IPL_Score_Predictor',layout="centered")
+
+#Get the ML model 
+
+filename='ml_model.pkl'
+model = pickle.load(open(filename,'rb'))
+
+#Title of the page with CSS
+
+st.markdown("<h1 style='text-align: center; color: white;'> IPL Score Predictor </h1>", unsafe_allow_html=True)
+
+#Add background image
+
+st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://images.unsplash.com/photo-1540747913346-19e32dc3e97e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
+
+#Add description
+
+with st.expander("**Description**"):
+    st.markdown("A Simple ML Model to predict IPL Scores between teams in an ongoing match. To make sure the model results accurate score and some reliability the minimum no. of current overs considered is greater than 5 overs.")
+
+# SELECT THE BATTING TEAM
+
+
+batting_team = st.selectbox("**Select the Batting Team**",('Chennai Super Kings', 'Delhi Daredevils', 'Kings XI Punjab','Kolkata Knight Riders','Mumbai Indians','Rajasthan Royals','Royal Challengers Bangalore','Sunrisers Hyderabad'))
+
+prediction_array = []
+  # Batting Team
+if batting_team == 'Chennai Super Kings':
+    prediction_array = prediction_array + [1,0,0,0,0,0,0,0]
+elif batting_team == 'Delhi Daredevils':
+    prediction_array = prediction_array + [0,1,0,0,0,0,0,0]
+elif batting_team == 'Kings XI Punjab':
+    prediction_array = prediction_array + [0,0,1,0,0,0,0,0]
+elif batting_team == 'Kolkata Knight Riders':
+    prediction_array = prediction_array + [0,0,0,1,0,0,0,0]
+elif batting_team == 'Mumbai Indians':
+    prediction_array = prediction_array + [0,0,0,0,1,0,0,0]
+elif batting_team == 'Rajasthan Royals':
+    prediction_array = prediction_array + [0,0,0,0,0,1,0,0]
+elif batting_team == 'Royal Challengers Bangalore':
+    prediction_array = prediction_array + [0,0,0,0,0,0,1,0]
+elif batting_team == 'Sunrisers Hyderabad':
+    prediction_array = prediction_array + [0,0,0,0,0,0,0,1]
+
+
+
+
+#SELECT BOWLING TEAM
+
+bowling_team = st.selectbox('**Select the Bowling Team**',('Chennai Super Kings', 'Delhi Daredevils', 'Kings XI Punjab','Kolkata Knight Riders','Mumbai Indians','Rajasthan Royals','Royal Challengers Bangalore','Sunrisers Hyderabad'))
+if bowling_team == batting_team:
+    st.error('**Bowling and Batting teams should be different**')
+# Bowling Team
+if bowling_team == 'Chennai Super Kings':
+    prediction_array = prediction_array + [1,0,0,0,0,0,0,0]
+elif bowling_team == 'Delhi Daredevils':
+    prediction_array = prediction_array + [0,1,0,0,0,0,0,0]
+elif bowling_team == 'Kings XI Punjab':
+    prediction_array = prediction_array + [0,0,1,0,0,0,0,0]
+elif bowling_team == 'Kolkata Knight Riders':
+    prediction_array = prediction_array + [0,0,0,1,0,0,0,0]
+elif bowling_team == 'Mumbai Indians':
+    prediction_array = prediction_array + [0,0,0,0,1,0,0,0]
+elif bowling_team == 'Rajasthan Royals':
+    prediction_array = prediction_array + [0,0,0,0,0,1,0,0]
+elif bowling_team == 'Royal Challengers Bangalore':
+    prediction_array = prediction_array + [0,0,0,0,0,0,1,0]
+elif bowling_team == 'Sunrisers Hyderabad':
+    prediction_array = prediction_array + [0,0,0,0,0,0,0,1]
+  
+
+col1, col2 = st.columns(2)
+
+#Enter the Current Ongoing Over
+with col1:
+    overs = st.number_input('**Enter the Current Over**',min_value=5.1,max_value=19.5,value=5.1,step=0.1)
+    if overs-math.floor(overs)>0.5:
+        st.error('Please enter valid over input as one over only contains 6 balls')
+with col2:
+#Enter Current Run
+    runs = st.number_input('**Enter Current runs**',min_value=0,max_value=354,step=1,format='%i')
+
+
+#Wickets Taken till now
+wickets =st.slider('**Enter Wickets fallen till now**',0,9)
+wickets=int(wickets)
+
+col3, col4 = st.columns(2)
+
+with col3:
+#Runs in last 5 over
+    runs_in_prev_5 = st.number_input('**Runs scored in the last 5 overs**',min_value=0,max_value=runs,step=1,format='%i')
+
+with col4:
+#Wickets in last 5 over
+    wickets_in_prev_5 = st.number_input('**Wickets taken in the last 5 overs**',min_value=0,max_value=wickets,step=1,format='%i')
+
+#Get all the data for predicting
+
+prediction_array = prediction_array + [runs, wickets, overs, runs_in_prev_5,wickets_in_prev_5]
+prediction_array = np.array([prediction_array])
+predict = model.predict(prediction_array)
+
+
+if st.button('Predict Score'):
+    #Call the ML Model
+    my_prediction = int(round(predict[0]))
+    
+    #Display the predicted Score Range
+    x=f'**PREDICTED MATCH SCORE : {my_prediction-5} to {my_prediction+5}**' 
+    st.success(x)
+
+
+
+teams = ['Sunrisers Hyderabad',
+ 'Mumbai Indians',
+ 'Royal Challengers Bangalore',
+ 'Kolkata Knight Riders',
+ 'Kings XI Punjab',
+ 'Chennai Super Kings',
+ 'Rajasthan Royals',
+ 'Delhi Capitals']
+
+cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi',
+       'Chandigarh', 'Jaipur', 'Chennai', 'Cape Town', 'Port Elizabeth',
+       'Durban', 'Centurion', 'East London', 'Johannesburg', 'Kimberley',
+       'Bloemfontein', 'Ahmedabad', 'Cuttack', 'Nagpur', 'Dharamsala',
+       'Visakhapatnam', 'Pune', 'Raipur', 'Ranchi', 'Abu Dhabi',
+       'Sharjah', 'Mohali', 'Bengaluru']
+
+pipe = pickle.load(open('pipe.pkl','rb'))
+st.title('IPL Win Predictor')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    batting_team = st.selectbox('Select the batting team',sorted(teams))
+with col2:
+    bowling_team = st.selectbox('Select the bowling team',sorted(teams))
+
+selected_city = st.selectbox('Select host city',sorted(cities))
+
+target = st.number_input('Target')
+
+col3,col4,col5 = st.columns(3)
+
+with col3:
+    score = st.number_input('Score')
+with col4:
+    overs = st.number_input('Overs completed')
+with col5:
+    wickets = st.number_input('Wickets out')
+
+if st.button('Predict Probability'):
+    runs_left = target - score
+    balls_left = 120 - (overs*6)
+    wickets = 10-(wickets)
+    crr = score/overs
+    rrr = (runs_left*6)/balls_left
+
+    input_df = pd.DataFrame({'batting_team':[batting_team],'bowling_team':[bowling_team],'city':[selected_city],'runs_left':[runs_left],'balls_left':[balls_left],'wickets_left':[wickets],'total_runs_x':[target],'crr':[crr],'rrr':[rrr]})
+
+    result = pipe.predict_proba(input_df)
+    loss = result[0][0]
+    win = result[0][1]
+    st.header(batting_team + "- " + str(round(win*100)) + "%")
+    st.header(bowling_team + "- " + str(round(loss*100)) + "%")
